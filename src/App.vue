d0f2e9a98f28920893ac5d45f3db35ec5b3126cc
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-02-05 10:40:22 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,12 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+
 .side-panel {
-  position: fixed; /* Фиксируем боковую панель */
-  top: 0; /* Прижимаем к верхнему краю */
-  left: 0; /* Прижимаем к левому краю (если панель слева) */
-  width: 250px; /* Укажите нужную ширину */
-  height: 100vh; /* Растягиваем на всю высоту экрана */
+  position: sticky;
+  top: 10px; /* Панель "прилипнет" к верхнему краю родительского контейнера */
+  height: fit-content; /* Высота подстраивается под контент */
   background: rgb(241, 241, 241);
   padding: 10px;
   border-radius: 5px;
-  overflow-y: auto; /* Позволяет прокручивать содержимое внутри панели */
 }
