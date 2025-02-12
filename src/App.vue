bb3051d22608bc19fcf342ac058e44af97db2e61
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-02-12 15:28:21 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,44 @@ export default defineComponent({
 })
 </script>
 
-    
-.month-wrapper {
+.header-content {
   display: flex;
-  align-items: center; /* Выровнять элементы по центру */
-  flex-wrap: nowrap;
-}
-
-.expansion-item-wrapper {
-  display: flex;
-  align-items: center; /* Чтобы label и иконка были на одной линии */
+  align-items: center;
+  width: 100%;
 }
 
 .adaptation-process-state {
   display: flex;
-  align-items: center; /* Выровнять текст и иконку по центру */
-  margin-left: auto; /* Сдвинуть вправо */
+  align-items: center;
+  margin-left: auto; /* Смещаем вправо */
 }
+
+.adaptation-state-value {
+  margin-right: 8px; /* Отступ между текстом и иконкой */
+}
+<q-expansion-item
+  expand-separator
+  switch-toggle-side
+  label="Первый месяц работы"
+  class="expansion-item-wrapper"
+>
+  <template v-slot:header>
+    <div class="header-content">
+      <span>Первый месяц работы</span>
+      <div class="adaptation-process-state row">
+        <p class="adaptation-state-value">Выполнено</p>
+        <q-icon size="md" name="fa-solid fa-square-check" color="green" />
+      </div>
+    </div>
+  </template>
+
+  <q-card>
+    <q-card-section class="main-process-container column">
+      <ul class="table-list">
+        <li class="table-row-item column">
+          ОЛтклтпу уптушкпщшуп упштущктпшщукп...
+        </li>
+      </ul>
+    </q-card-section>
+  </q-card>
+</q-expansion-item>
