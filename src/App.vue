ab634e5bbd6e303eedfa9d69dfa89858098d5d1e
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-02-12 16:53:32 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,46 +10,51 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'App'
 })
+
+<template>
+  <ul class="table-list">
+    <li class="table-row-item column">
+      РВииггаи тщкуткш кп33 55 кптшк кпшткп кштпктш
+    </li>
+
+    <li class="table-row-item column">
+      <q-toggle :false-value="false" :true-value="true" label="Не выполнено" color="green" unchecked-color="red" />
+      <q-toggle :false-value="false" :true-value="true" label="Выполнено" color="green" unchecked-color="red" />
+    </li>
+
+    <li class="table-row-item column">
+      <q-toggle :false-value="false" :true-value="true" label="Не выполнено" color="green" disable />
+      <q-toggle :false-value="false" :true-value="true" label="Не выполнено" color="green" disable />
+    </li>
+
+    <li class="table-row-item">
+      12.04.2025 <br />
+      17.04.2025
+    </li>
+
+    <!-- Date Picker -->
+    <li class="table-row-item date-pick">
+      <q-input v-model="selectedDate" filled readonly label="Выберите дату">
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer" @click="showDatePicker = true" />
+        </template>
+
+        <q-popup-proxy v-model="showDatePicker" transition-show="scale" transition-hide="scale">
+          <q-date v-model="selectedDate" mask="DD.MM.YYYY" @update:model-value="showDatePicker = false" />
+        </q-popup-proxy>
+      </q-input>
+    </li>
+
+    <li class="table-row-item">
+      цель выполнена тысытв ымвтгым мывгштгвытм мвмт
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const selectedDate = ref('23.03.2025') // Начальная дата
+const showDatePicker = ref(false)
 </script>
 
-.header-content {
-  display: flex;
-  align-items: center;
-  width: 100%;
-}
-
-.adaptation-process-state {
-  display: flex;
-  align-items: center;
-  margin-left: auto; /* Смещаем вправо */
-}
-
-.adaptation-state-value {
-  margin-right: 8px; /* Отступ между текстом и иконкой */
-}
-<q-expansion-item
-  expand-separator
-  switch-toggle-side
-  label="Первый месяц работы"
-  class="expansion-item-wrapper"
->
-  <template v-slot:header>
-    <div class="header-content">
-      <span>Первый месяц работы</span>
-      <div class="adaptation-process-state row">
-        <p class="adaptation-state-value">Выполнено</p>
-        <q-icon size="md" name="fa-solid fa-square-check" color="green" />
-      </div>
-    </div>
-  </template>
-
-  <q-card>
-    <q-card-section class="main-process-container column">
-      <ul class="table-list">
-        <li class="table-row-item column">
-          ОЛтклтпу уптушкпщшуп упштущктпшщукп...
-        </li>
-      </ul>
-    </q-card-section>
-  </q-card>
-</q-expansion-item>
