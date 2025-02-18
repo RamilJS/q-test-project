ed13f523d7f5077df34b9784d7f1021d1025810a
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-02-18 19:15:09 +0300

    Update ErrorNotFound.vue

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -63,5 +63,21 @@ async postTaskState(newValue, taskId) {
   }
 }
 
+  async handleToggle(newValue, id) {
+  await this.postTaskState(newValue, id);
+  this.updateLocalTask(id, newValue);
+},
+
+updateLocalTask(taskId, newValue) {
+  this.cabinetData.forEach(process => {
+    process.tasks.forEach(task => {
+      if (task.id === taskId) {
+        task.completed = newValue;
+      }
+    });
+  });
+}
+
+
 
 </script>
