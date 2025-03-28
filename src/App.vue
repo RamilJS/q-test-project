9f6688a7a3e6bbb5b47c0f0be87ad594fc7d9029
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-03-28 13:28:38 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -29,3 +29,24 @@
     </q-table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      columns: [
+        { name: 'fio', label: 'ФИО', align: 'left', field: 'fio' },
+        { name: 'position', label: 'Должность', align: 'left', field: 'position' },
+        { name: 'subdivision', label: 'Подразделение', align: 'left', field: 'subdivision' },
+      ],
+      optionsCoworkers: [
+        { id: 1, fio: 'Иванов Алексей Игоревич', position: 'Управляющий отделением', subdivision: 'Название подразделения', selected: false },
+        { id: 2, fio: 'Иванов Андрей Алексеевич', position: 'Техник по планированию', subdivision: 'Название подразделения', selected: false },
+        { id: 3, fio: 'Иванова Анна Ивановна', position: 'Начальник отдела по защите', subdivision: 'Название подразделения', selected: false },
+        { id: 4, fio: 'Иванова Дарья Владимировна', position: 'Начальник отдела кадров', subdivision: 'Название подразделения', selected: false }
+      ]
+    };
+  }
+};
+</script>
+
