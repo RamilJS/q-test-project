ee9cca6fe01ec0521bbf5286173521879f918027
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-04-12 17:52:33 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,3 @@
     <router-view />
   </div>
 </template>
-
-const toggleExpansion = (refVar, key, value) => {
-  refVar.value = value;
-  sessionStorage.setItem(key, JSON.stringify(value));
-};
-
-<q-expansion-item
-  :model-value="isExpansionOpen1"
-  @update:model-value="val => toggleExpansion(isExpansionOpen1, 'isExpansionOpen1', val)"
-  ...
->
