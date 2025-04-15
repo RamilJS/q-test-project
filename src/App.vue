40a500851780b71e6a3cc98fd1d1a37aa02cf6f3
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-04-15 13:07:53 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,14 @@ const copyEmployeeStatuses = () => {
 
   showToast("Статусы скопированы");
 };
+
+const copyEmployeeStatuses = async () => {
+  for (const task of cabinetData.value) {
+    // Только если значения отличаются, чтобы не дергать лишние запросы
+    if (task.completed !== task.completedMentor) {
+      await handleToggle(task.completed, task.id, task); // здесь `task` — это и есть "month", тк нет структуры месяцев
+    }
+  }
+
+  showToast("Статусы скопированы");
+};
