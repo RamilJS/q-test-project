00d4b9c7e998e51b65658559098f38b518d50846
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-04-23 10:56:08 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,23 @@
   </div>
 </template>
 
-<!-- Поле поиска коллег -->
 <div class="employee-search-container">
   <span class="text-bold">Рекомендации коллег</span>
-
-  <q-input
-    v-model="collaboratorSearch"
-    @update:model-value="fetchCollaboratorSearch"
-    dense
-    outlined
-    placeholder="Поиск"
-    class="bg-white col"
-    style="margin-top: 8px"
-  />
-</div>
-
-<!-- Список рекомендованных коллег -->
-<div class="q-pa-md" style="padding-left: 0; padding-right: 0;">
-  <div
-    v-for="collaborator in collaboratorListData"
-    :key="collaborator.id"
-    class="q-pa-sm q-mb-sm q-border q-rounded bg-white"
-    style="display: flex; align-items: flex-start;"
-  >
-    <q-radio
-      v-model="selectedCoworkerId"
-      :val="collaborator.id"
-      color="primary"
-      class="q-mr-md"
+  <div class="row q-gutter-sm" style="margin-top: 8px;">
+    <q-input
+      v-model="collaboratorSearch"
+      @keyup.enter="fetchCollaboratorSearch"
+      dense
+      outlined
+      placeholder="Поиск"
+      class="bg-white col"
     />
-    <div>
-      <div><strong>{{ collaborator.fullname }}</strong></div>
-      <div>{{ collaborator.position_name }}</div>
-      <div>{{ collaborator.department_name }}</div>
-    </div>
+    <q-btn
+      icon="search"
+      color="primary"
+      flat
+      @click="fetchCollaboratorSearch"
+    />
   </div>
 </div>
 
