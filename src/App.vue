ff2f43358cad8082a674ab70bf9eb105ae468564
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-05-27 09:55:16 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
-const copyToClipboard = async (taskId, path) => {
-  try {
-    await navigator.clipboard.writeText(path);
-    copiedState.value[taskId] = true;
+<li class="table-row-item" style="position: relative">
+  <p style="margin-bottom: 10px">Название документа</p>
 
-    setTimeout(() => {
-      copiedState.value[taskId] = false;
-    }, 3000);
-  } catch (err) {
-    console.error("Ошибка при копировании: ", err);
-  }
-};
+  <span v-if="copiedState[infoTask.id]" class="copied-message">
+    Ссылка успешно скопирована!
+  </span>
+
+  <div>
+    <q-btn
+      v-if="infoTask.path !== ''"
+      flat
+      unelevated
+      size="sm"
+      padding="none"
+      color="secondary"
+      @click="copyToClipboard(infoTask.id, infoTask.path)"
+      class="info-task-link"
+    >
+      Копировать ссылку для ознакомления
+    </q-btn>
+    <span v-else>{{ infoTask.pathText }}</span>
+  </div>
+</li>
