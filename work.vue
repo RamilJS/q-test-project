92bd70f67d1f24305e56187e18e875653f4d14a5
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-05-29 12:29:04 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,17 @@
+<!-- Удаляем старую кнопку "Отправить" в верхней части -->
+
+<!-- Новый блок в самом низу -->
+<q-card-actions align="right">
+  <q-btn
+    flat
+    label="Закрыть"
+    color="primary"
+    v-close-popup
+  />
+  <q-btn
+    label="Отправить"
+    color="primary"
+    @click="postNewMeeting(employee.id)"
+  />
+</q-card-actions>
 
