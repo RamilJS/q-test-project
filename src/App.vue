3b68138fcef674ac5e77f979d2d82d0b3510725a
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-05-30 15:20:28 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
 
+<q-btn
+  v-if="employee.mentor.person_fullname && (employee.status == 'active' || employee.status == 'planned')"
+  label="Удалить помощника"
+  flat
+  color="primary"
+  class="delete-mentor-button"
+  size="sm"
+  padding="sm"
+  @click="openConfirmDeleteMentor(employee.id)"
+  style="min-width: 145px;"
+/>
+
+<q-btn
+  v-if="employee.status == 'active' || employee.status == 'planned'"
+  color="primary"
+  text-color="white"
+  label="Назначить помощника"
+  size="sm"
+  padding="sm"
+  @click="openAppointAssistantModal(employee.person.person_id)"
+/>
