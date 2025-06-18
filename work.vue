9aab3f2b684bd0f6dec22df86f10003cab36e1ca
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-06-18 11:54:19 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,62 +1,32 @@
-const getUserRoles = (usersString) => {
-  if (!usersString) return [];
-  return usersString.split(';').map((role) => {
-    switch (role.trim()) {
-      case 'manager':
-        return 'Руководитель';
-      case 'collaborator':
-        return 'Сотрудник';
-      case 'mentor':
-        return 'Наставник';
-      default:
-        return role; // на случай непредвиденных ролей
-    }
-  });
+const isCoworkersSuccessModalOpen = ref(false);
+
+<q-dialog v-model="isCoworkersSuccessModalOpen">
+  <q-card style="min-width: 500px">
+    <q-card-section class="text-h6">
+      {{ coworkersMessage }}
+    </q-card-section>
+
+    <q-card-actions align="right">
+      <q-btn label="Закрыть" flat color="primary" @click="isCoworkersSuccessModalOpen = false" />
+      <q-btn
+        label="Порекомендовать ещё коллегу"
+        color="primary"
+        @click="recommendAnotherCoworker"
+      />
+    </q-card-actions>
+  </q-card>
+</q-dialog>
+
+const recommendAnotherCoworker = async () => {
+  isCoworkersSuccessModalOpen.value = false;
+  await nextTick(); // чтобы гарантировать закрытие предыдущего окна
+  openCoworkersModal(); // снова открываем форму
 };
 
-const getTooltipText = (task) => {
-  if (!task.users) {
-    return 'Задача сохраняется автоматически';
-  }
-
-  const roles = getUserRoles(task.users);
-  if (roles.length === 0) {
-    return 'Задача сохраняется автоматически';
-  }
-
-  return `Данное поле заполняется ${roles.join(', ')}`;
-};
-
-const getRoleColor = (role) => {
-  switch (role.trim()) {
-    case 'mentor':
-      return 'blue';
-    case 'collaborator':
-      return 'green';
-    case 'manager':
-      return 'red';
-    default:
-      return 'grey';
-  }
-};
-
-<!-- Отображаем блок автора действия только если task.users не пустой -->
-<div
-  v-if="task.users"
-  class="manager-title"
-  style="
-    margin-left: 10px;
-    background-color: #ddd;
-    border-radius: 10px;
-    width: fit-content;
-    padding: 0 5px;
-  "
->
-  <template v-for="role in task.users.split(';')" :key="role">
-    <q-icon :color="getRoleColor(role)" size="8px" name="fa-solid fa-circle" />
-    <span class="manager-title-item" style="margin-left: 5px; font-size: 12px;">
-      {{ getUserLabel(role) }}
-    </span>
-  </template>
-</div>
-
+saveCoworkersRecommendation()
+newComment.value.text = "";
+selectedCoworkerId.value = null;
+collaboratorSearch.value = "";
+isCoworkersModalOpen.value = false;
+await fetchCollaboratorSearch();
+isCoworkersSuccessModalOpen.value = true;
