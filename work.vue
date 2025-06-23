6bf14215e8a8abc9dd608a343c0f3a74e0684401
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-06-23 11:53:18 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,11 @@
 
+const submitComment = async (taskId) => {
+  try {
+    await postCommentState(taskId);
+    await fetchCabinetData();
+    showToast('Комментарий успешно добавлен', 'positive');
+  } catch (error) {
+    showToast('Ошибка при отправке комментария или обновлении данных', 'negative');
+    console.error('Ошибка в submitComment:', error);
+  }
+};
