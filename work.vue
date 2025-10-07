df038dbfc3abe1784104d78751cc1304be766f8a
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-10-07 14:23:01 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,15 +1,1 @@
-onMounted(() => {
-  if (!getCookie(cookieName)) {
-    startGuide();
-    window.addEventListener("resize", positionTooltip);
-  } else {
-    // ✅ гарантированно убираем рамку, если гид уже был показан
-    removeHighlight();
-  }
-});
 
-onUnmounted(() => {
-  window.removeEventListener("resize", positionTooltip);
-  clearTimeout(guideTimeout);
-  removeHighlight(); // ✅ на всякий случай
-});
