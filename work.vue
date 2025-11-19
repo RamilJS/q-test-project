b27c832a7984d96160c58d5cb93ba5ef5ec6ffda
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-11-19 16:23:48 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,26 +1,25 @@
-// personId и requestTypeId — входные параметры (если requestTypeId == '' — не фильтруем по типу)
+// параметры (подставь свои / бери из Param)
 var personId = OptInt(Param.personId || curUserID, 0);
-var requestTypeId = (Param.requestTypeId || ""); // пример: 7194701844403546882
+var requestTypeId = (Param.requestTypeId || ""); // например "0x63D8B73554856B02"
 
+// строим SQL
 var bSqlStr = new Binary();
 
 bSqlStr.AppendStr("SELECT \r\n");
 bSqlStr.AppendStr("  r.id, \r\n");
 bSqlStr.AppendStr("  r.create_date, \r\n");
 bSqlStr.AppendStr("  r.request_type_id, \r\n");
-// вытаскиваем кастомное поле f_vacancy_name из XML-хранилища колонки data
-bSqlStr.AppendStr("  r.[data].value('(request/custom_elems/custom_elem[name=\"f_vacancy_name\"]/value)[1]', 'varchar(max)') AS f_vacancy_name, \r\n");
-// вытаскиваем кастомное поле f_candidate_function
-bSqlStr.AppendStr("  r.[data].value('(request/custom_elems/custom_elem[name=\"f_candidate_function\"]/value)[1]', 'varchar(max)') AS f_candidate_function \r\n");
+// правильно: двойные одинарные кавычки вокруг имени custom_elem
+bSqlStr.AppendStr("  r.[data].value('(request/custom_elems/custom_elem[name=''f_vacancy_name'']/value)[1]', 'varchar(max)') AS f_vacancy_name, \r\n");
+bSqlStr.AppendStr("  r.[data].value('(request/custom_elems/custom_elem[name=''f_candidate_function'']/value)[1]', 'varchar(max)') AS f_candidate_function \r\n");
 bSqlStr.AppendStr("FROM requests r \r\n");
 bSqlStr.AppendStr("WHERE r.person_id = " + Int(personId) + " \r\n");
 
 if (requestTypeId != "") {
-  // если requestTypeId передано — фильтруем по нему
   bSqlStr.AppendStr("  AND r.request_type_id = " + SqlLiteral(requestTypeId) + " \r\n");
 }
 
 bSqlStr.AppendStr("ORDER BY r.create_date DESC \r\n");
 
-// выполняем
+// Выполнение
 var aResults = XQuery("sql:" + bSqlStr.GetStr());
