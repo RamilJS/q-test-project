4ac87d56d6d8999d2bf4d150ef65b68bd9c2c561
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-11-19 15:16:59 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,4 +1,4 @@
-iPersonID = curUserID;  // текущий пользователь
+iPersonID = curUserID;
 
 bSqlStr = new Binary();
 
@@ -6,17 +6,18 @@ bSqlStr.AppendStr("select \r\n");
 bSqlStr.AppendStr("    r.id, \r\n");
 bSqlStr.AppendStr("    r.person_id, \r\n");
 bSqlStr.AppendStr("    r.create_date, \r\n");
-bSqlStr.AppendStr("    ce1.value as f_vacancy_name, \r\n");
-bSqlStr.AppendStr("    ce2.value as f_candidate_function \r\n");
+bSqlStr.AppendStr("    ce_name.value as f_vacancy_name, \r\n");
+bSqlStr.AppendStr("    ce_func.value as f_candidate_function \r\n");
 bSqlStr.AppendStr("from requests r \r\n");
 
-bSqlStr.AppendStr("left join request_custom_elems ce1 \r\n");
-bSqlStr.AppendStr("    on ce1.request_id = r.id and ce1.name = 'f_vacancy_name' \r\n");
+bSqlStr.AppendStr("left join request_custom_elems ce_name \r\n");
+bSqlStr.AppendStr("    on ce_name.request_id = r.id \r\n");
+bSqlStr.AppendStr("    and ce_name.name = 'f_vacancy_name' \r\n");
 
-bSqlStr.AppendStr("left join request_custom_elems ce2 \r\n");
-bSqlStr.AppendStr("    on ce2.request_id = r.id and ce2.name = 'f_candidate_function' \r\n");
+bSqlStr.AppendStr("left join request_custom_elems ce_func \r\n");
+bSqlStr.AppendStr("    on ce_func.request_id = r.id \r\n");
+bSqlStr.AppendStr("    and ce_func.name = 'f_candidate_function' \r\n");
 
-bSqlStr.AppendStr("where r.person_id = " + Int(iPersonID) + "\r\n");
+bSqlStr.AppendStr("where r.person_id = " + Int(iPersonID) + " \r\n");
 
 aTempArray = XQuery("sql:" + bSqlStr.GetStr());
-
