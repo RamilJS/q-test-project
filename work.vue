6c7fb39ed023e3c7b841c5fac9dd9a1fccae4d2b
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2025-12-01 16:40:25 +0300

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,28 +1,39 @@
-iPersonID = curUserID;  // текущий пользователь
-requestTypeID = "7194701844403546882";
+// ID подразделения из заявки
+var subdivisionId = OptInt(oEventDocTE.custom_elems.ObtainChildByKey("f_vacancy_subdivision_id").value);
 
-sSearchStr = Trim(search);
+// Объект результата
+var oOrgObj = new Object();
+oOrgObj.id = undefined;
+oOrgObj.name = undefined;
 
-bSqlStr = new Binary();
+if (subdivisionId > 0) {
 
-bSqlStr.AppendStr("with fcte as (\r\n");
-bSqlStr.AppendStr("    select r.id,\r\n");
-bSqlStr.AppendStr("           r.create_date,\r\n");
-bSqlStr.AppendStr("           r.request_type_id,\r\n");
-bSqlStr.AppendStr("           r.data.value('(*/custom_elems/custom_elem[name=''f_vacancy_name'']/value)[1]', 'varchar(max)') as f_vacancy_name,\r\n");
-bSqlStr.AppendStr("           r.data.value('(*/custom_elems/custom_elem[name=''f_candidate_function'']/value)[1]', 'varchar(max)') as f_candidate_function\r\n");
-bSqlStr.AppendStr("    from requests r\r\n");
-bSqlStr.AppendStr("    where r.person_id = " + OptInt(iPersonID) + "\r\n");
-bSqlStr.AppendStr("      and r.request_type_id = " + OptInt(requestTypeID) + "\r\n");
-bSqlStr.AppendStr(")\r\n");
+    // 1. Ищем подразделение
+    var subArr = XQuery(
+        "for $s in subdivisions " +
+        "where $s/id = " + subdivisionId + " " +
+        "return $s"
+    );
 
-bSqlStr.AppendStr("select id, create_date, request_type_id\r\n");
-bSqlStr.AppendStr("from fcte\r\n");
+    if (ArrayCount(subArr) > 0) {
 
-if (sSearchStr != '') {
-    sSearchStr = SqlLiteral('%' + sSearchStr + '%'); // защита от SQL-инъекций
-    bSqlStr.AppendStr("where f_vacancy_name like " + sSearchStr + "\r\n");
-    bSqlStr.AppendStr("   or f_candidate_function like " + sSearchStr + "\r\n");
+        var sub = subArr[0];
+
+        var orgId = OptInt(sub.org_id);
+
+        if (orgId > 0) {
+
+            // 2. Ищем организацию по org_id
+            var orgArr = XQuery(
+                "for $o in organizations " +
+                "where $o/id = " + orgId + " " +
+                "return $o"
+            );
+
+            if (ArrayCount(orgArr) > 0) {
+                oOrgObj.id = orgArr[0].id;
+                oOrgObj.name = String(orgArr[0].name);
+            }
+        }
+    }
 }
-
-aTempArray = XQuery("sql:" + bSqlStr.GetStr());
