c057dd66272fc173795daa893f2d699208494620
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-02-24 18:39:46 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,21 +1,33 @@
 var finalDateStr = String(oEventDocTE.custom_elems.ObtainChildByKey("f_final_date").value);
-var currentYear = Int(StrLeft(String(curEnv.nowDate), 4));
+
+// получаем текущий год
+var currentYear = Int(StrLeft(StrDate(CurDate, false), 4));
+
+var showYearResult = false;
 
 if (finalDateStr != "") {
 
+    var finalYear = Int(StrLeft(finalDateStr, 4));
+
+    if (finalYear != currentYear) {
+        showYearResult = true;
+    }
+}
+
+// Финальная дата
+RESULT.push({
+    "name": "Финальная дата завершения:",
+    "value": finalDateStr,
+    "id": ArrayCount(RESULT)
+});
+
+// Только если год НЕ равен текущему
+if (showYearResult) {
+
     RESULT.push({
-        "name": "Финальная дата завершения:",
-        "value": finalDateStr,
+        "name": "Ожидаемый результат на конец текущего года (критерии успешности):",
+        "value": String(oEventDocTE.custom_elems.ObtainChildByKey("f_year_result").value),
         "id": ArrayCount(RESULT)
     });
 
-    if (Int(StrLeft(finalDateStr, 4)) != currentYear) {
-
-        RESULT.push({
-            "name": "Ожидаемый результат на конец текущего года (критерии успешности):",
-            "value": String(oEventDocTE.custom_elems.ObtainChildByKey("f_year_result").value),
-            "id": ArrayCount(RESULT)
-        });
-
-    }
 }
