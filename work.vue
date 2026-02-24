6d97225f358802cb32ae453afbf2b4955cc296fc
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-02-24 19:01:15 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,31 +1,30 @@
-var finalDate = oEventDocTE.custom_elems.ObtainChildByKey("f_final_date").value;
-var showYearResult = false;
+var finalDateElem = oEventDocTE.custom_elems.ObtainChildByKey("f_final_date");
+var yearResultElem = oEventDocTE.custom_elems.ObtainChildByKey("f_year_result");
 
+var finalDate = finalDateElem != undefined ? finalDateElem.value : undefined;
+
+// всегда показываем финальную дату
+RESULT.push({
+    "name": "Финальная дата завершения:",
+    "value": finalDate ? StrDate(finalDate) : "",
+    "id": ArrayCount(RESULT)
+});
+
+
+// проверяем нужно ли показывать year_result
 if (finalDate != undefined && finalDate != null && finalDate != "") {
 
     var finalYear = Year(finalDate);
     var currentYear = Year(Date());
 
     if (finalYear != currentYear) {
-        showYearResult = true;
+
+        RESULT.push({
+            "name": "Ожидаемый результат на конец текущего года:",
+            "value": yearResultElem != undefined ? ("" + yearResultElem.value) : "",
+            "id": ArrayCount(RESULT)
+        });
+
     }
-
-}
-
-// Финальная дата
-RESULT.push({
-    "name": "Финальная дата завершения:",
-    "value": StrDate(finalDate),
-    "id": ArrayCount(RESULT)
-});
-
-// Показываем только если год НЕ текущий
-if (showYearResult) {
-
-    RESULT.push({
-        "name": "Ожидаемый результат на конец текущего года:",
-        "value": "" + oEventDocTE.custom_elems.ObtainChildByKey("f_year_result").value,
-        "id": ArrayCount(RESULT)
-    });
 
 }
