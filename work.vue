fb11481a0b7ce27dd5f8a67a5a5d7f185e55e9c9
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-02-25 12:45:02 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,32 +1,40 @@
-// получаем финальную дату из XML (строка)
-var finalDateStr = oEventDocTE.custom_elems.ObtainChildByKey("f_final_date").value;
-
-// текущий год
+// получаем финальную дату
+var finalDateStr = getFormField("finalDate", "");
 var currentYear = Year(Date());
-
-// флаг показа поля
 var showYearResult = false;
 
-// проверяем, что дата есть и не пустая
 if (finalDateStr != undefined && finalDateStr != null && finalDateStr != "") {
 
-    // Берем первые 4 символа строки — это год
+    // finalDate приходит как 2027-02-27T00:00:00+00:00
     var finalYearStr = String(finalDateStr).substr(0,4);
-
-    // преобразуем в число
     var finalYear = Int(finalYearStr);
 
-    // показываем поле f_year_result только если год финальной даты НЕ совпадает с текущим
-    if (finalYear != currentYear) {
+    // показываем только если год больше текущего
+    if (finalYear > currentYear) {
         showYearResult = true;
     }
 }
 
-// добавляем поле только если нужно
+// поле "Ожидаемый результат на финальную дату"
+oForm.form_fields.push({
+    name: "result",
+    label: "Ожидаемый результат на финальную дату завершения (критерии успешности)",
+    type: "text",
+    richtext: true,
+    value: getFormField("result", ""),
+    mandatory: false
+});
+
+// добавляем yearResult ТОЛЬКО если finalDate > текущего года
 if (showYearResult) {
-    RESULT.push({
-        "name": "Ожидаемый результат на конец текущего года (критерии успешности):",
-        "value": String(oEventDocTE.custom_elems.ObtainChildByKey("f_year_result").value),
-        "id": ArrayCount(RESULT)
+
+    oForm.form_fields.push({
+        name: "yearResult",
+        label: "Ожидаемый результат на конец текущего года (критерии успешности)",
+        type: "text",
+        richtext: true,
+        value: getFormField("yearResult", ""),
+        mandatory: false
     });
+
 }
