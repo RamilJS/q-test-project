86aafd607aef19cd3cd58adedd6b550b6903d13c
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-02-27 23:27:28 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,29 +1,21 @@
+// вычисляем флаг видимости yearResult
+var yearResultVisibleFlag = "0";
+
+var finalDateStr = getFormField("finalDate", "");
+
+if (finalDateStr != "")
+{
+    var currentYear = Year(Date());
+    var finalYear = Int(String(finalDateStr).substr(0,4));
+
+    if (finalYear > currentYear)
+        yearResultVisibleFlag = "1";
+}
+
 {
     name: "yearResultVisibleFlag",
     type: "string",
-    value: (function () {
-        var finalDateStr = getFormField("finalDate", "");
-        if (finalDateStr == "")
-            return "0";
-
-        var currentYear = Year(Date());
-        var finalYear = Int(String(finalDateStr).substr(0,4));
-
-        return (finalYear > currentYear ? "1" : "0");
-    })(),
-    visibility: false,
-    page: "page_one"
-},
-
-{
-    name: "yearResult",
-    label: "Ожидаемый результат на конец текущего года (критерии успешности)",
-    type: "text",
-    richtext: true,
-    value: getFormField("yearResult", ""),
-    mandatory: false, // важно: mandatory делаем через проверку, не тут
-    visibility: [
-        { parent: "yearResultVisibleFlag", value: "1" }
-    ],
+    value: yearResultVisibleFlag,
+    visibility: true,
     page: "page_one"
 },
