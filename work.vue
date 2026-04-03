369efa34a76e54c8e51e3c3d43bb0d59780ea065
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-04-03 17:35:27 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,3 +1,6 @@
+var person_id = "0x" + StrHex(OptInt(raw_person_id));
+var ass_id = "0x" + StrHex(OptInt(raw_ass_id));
+
 var arr = XQuery(
     "for $l in (active_test_learnings, test_learnings) " +
     "where $l/person_id = " + person_id +
@@ -6,3 +9,6 @@ var arr = XQuery(
     " and $l/start_usage_date <= date('" + StrDate(dFinishDate,false) + "')" +
     " return $l"
 );
+
+alert("person_id HEX = " + person_id);
+alert("ass_id HEX = " + ass_id);
