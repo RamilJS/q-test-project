e634bda48eefc809ff92b31d74e44519d0d72450
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-04-03 09:51:15 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,19 +1,1 @@
-function normalizeToArray(val)
-{
-    if (val == undefined || val == "")
-        return [];
 
-    // если уже массив
-    if (val instanceof Array)
-        return val;
-
-    // если строка с разделителем
-    if (StrContains(val, ";"))
-        return StrSplit(val, ";");
-
-    // если одиночное значение
-    return [val];
-}
-
-var oSelect_collaborator_ids = normalizeToArray(getFormField('select_collaborator_ids', ""));
-var oSelect_assessment_ids = normalizeToArray(getFormField('select_assessment_ids', ""));
