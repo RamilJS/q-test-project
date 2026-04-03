b4e64aefe9d293767a3236e320f694658d9bfc5a
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-04-03 10:58:25 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,23 @@
+var raw_person_id = aCollaboratorIDArray[i];
 
+if (raw_person_id == undefined)
+    continue;
+
+raw_person_id = Trim(raw_person_id);
+
+if (raw_person_id == "")
+    continue;
+
+var person_id = OptInt(raw_person_id);
+
+var raw_ass_id = aAssessmentIDArray[j];
+
+if (raw_ass_id == undefined)
+    continue;
+
+raw_ass_id = Trim(raw_ass_id);
+
+if (raw_ass_id == "")
+    continue;
+
+var ass_id = OptInt(raw_ass_id);
