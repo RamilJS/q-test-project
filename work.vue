ead55990eaca1bc1a4902cb0ad6a3ecd1ffffb37
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-04-15 18:38:45 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,3 +1,4 @@
+// --- DEBUG ---
 alert("ObjectType(history) = " + ObjectType(history));
 alert("ObjectType(history.objects) = " + ObjectType(history.objects));
 alert("ArrayCount(history.objects) = " + ArrayCount(history.objects));
@@ -7,3 +8,4 @@ alert("firstObj = " + firstObj);
 alert("ObjectType(firstObj) = " + ObjectType(firstObj));
 alert("firstObj.section = " + firstObj.section);
 alert("ObjectType(firstObj.section) = " + ObjectType(firstObj.section));
+// --- END DEBUG ---
