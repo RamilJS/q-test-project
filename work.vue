abfe7de1077037d6c32ef711710446bfa2c3a16d
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-04-16 14:06:07 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,7 +1,13 @@
-var qid2 = aQuestions[qh3].id;
-var qData = K.questions[qid2];
-if (qData === undefined && typeof qid2 === 'number') {
-    qData = K.questions[String(qid2)];
-} else if (qData === undefined && typeof qid2 === 'string') {
-    qData = K.questions[Number(qid2)];
+for (var qh3 = 0; qh3 < ArrayCount(aQuestions); qh3++) {
+    var qid2 = aQuestions[qh3].id;
+    var qData = K.questions.GetOptChildByKey(qid2);   // безопасный доступ
+    if (qData == undefined) {
+        html.AppendStr("<td colspan='4'> </td>");
+    } else {
+        var bg = qData.result == "неверно" ? "#F4CCCC" : "#D9EAD3";
+        html.AppendStr("<td width='150'>" + qData.quest_type + "</td>");
+        html.AppendStr("<td width='80' align='center' bgcolor='" + bg + "'>" + qData.result + "</td>");
+        html.AppendStr("<td width='250'>" + qData.answer + "</td>");
+        html.AppendStr("<td width='250'>" + qData.correct_answer + "</td>");
+    }
 }
