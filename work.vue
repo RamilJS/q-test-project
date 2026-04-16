b1093e9ef724cd8adc84c93e5abebcf43e273cf3
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-04-16 14:03:48 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,27 +1,7 @@
-// --- Заголовок вопросов ---
-html.AppendStr("<tr><td colspan='9'></td>");
-for (var qh_idx = 0; qh_idx < ArrayCount(aQuestions); qh_idx++) {
-    var qh_obj = aQuestions[qh_idx];
-    html.AppendStr("<td colspan='4' bgcolor='#EA9999'><b>" + (qh_obj.text || "") + "</b></td>");
+var qid2 = aQuestions[qh3].id;
+var qData = K.questions[qid2];
+if (qData === undefined && typeof qid2 === 'number') {
+    qData = K.questions[String(qid2)];
+} else if (qData === undefined && typeof qid2 === 'string') {
+    qData = K.questions[Number(qid2)];
 }
-html.AppendStr("</tr>");
-
-// --- Шапка ---
-html.AppendStr("<tr>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Пользователь</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Название теста</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Код</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Организация</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Подразделение</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Должность</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Дата</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Статус</b></td>");
-html.AppendStr("<td bgcolor='#FCE5CD'><b>Баллы</b></td>");
-
-for (var q_idx = 0; q_idx < ArrayCount(aQuestions); q_idx++) {
-    html.AppendStr("<td bgcolor='#FCE5CD'><b>Тип</b></td>");
-    html.AppendStr("<td bgcolor='#FCE5CD'><b>Результат</b></td>");
-    html.AppendStr("<td bgcolor='#FCE5CD'><b>Полученный ответ</b></td>");
-    html.AppendStr("<td bgcolor='#FCE5CD'><b>Правильный ответ</b></td>");
-}
-html.AppendStr("</tr>");
