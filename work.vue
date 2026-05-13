00fb2b73cd4659d4424a91b6e8c1d5f7ba204b6d
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-05-13 16:56:35 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,8 +1,35 @@
-copy(
-[...document.querySelectorAll('.WssListRow')]
-.map(r =>
-  [...r.querySelectorAll('.WssListCell')]
-  .map(c => c.innerText.trim())
-  .join(' | ')
-).join('\n')
-)
+const calendarEvents = computed(() => {
+
+  return datePicker.value.map((event) => ({
+
+    id: event.id,
+
+    title: event.name,
+
+    date: event.start_date
+      ? event.start_date.split("T")[0]
+      : "",
+
+    time: `${new Date(event.start_date).toLocaleTimeString(
+      "ru-RU",
+      {
+        hour: "2-digit",
+        minute: "2-digit",
+      }
+    )} - ${new Date(event.finish_date).toLocaleTimeString(
+      "ru-RU",
+      {
+        hour: "2-digit",
+        minute: "2-digit",
+      }
+    )}`,
+
+    link: event.link,
+
+    status: event.status_name,
+
+    image: event.image_url,
+
+  }));
+
+});
