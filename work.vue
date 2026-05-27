0af85fe7e788ca218e428daf83cd3c6c27219876
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-05-27 13:01:19 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,15 @@
+const onNavigation = ({ month, year }) => {
 
+  currentMonth.value = month + 1;
+  currentYear.value = year;
+
+  requestAnimationFrame(() => {
+
+    requestAnimationFrame(() => {
+
+      bindTooltips();
+
+    });
+
+  });
+};
