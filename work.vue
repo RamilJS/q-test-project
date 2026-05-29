de71ea7feda077e47cc1950ab3599071c65b94ee
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-05-29 13:54:40 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,16 +1,1 @@
-const eventDates = computed(() => {
 
-  return Object.keys(eventsMap.value)
-    .filter(Boolean)
-    .map(date => {
-
-      const [year, month, day] =
-        date.split("-");
-
-      return `${year}/${month}/${day}`;
-
-    });
-
-});
-
-console.log(eventDates.value);
