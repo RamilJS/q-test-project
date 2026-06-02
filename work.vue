ad6b9cc29a71dfe7dd79ee582ffde1618fec721c
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-06-02 17:15:36 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,16 @@
 
+<q-date
+  v-model="selectedDate"
+  flat
+  bordered
+  minimal
+  mask="YYYY-MM-DD"
+  color="primary"
+  class="custom-calendar"
+
+  :events="eventDates"
+
+  :event-color="getEventColor"
+
+  @update:model-value="onDateClick"
+/>
