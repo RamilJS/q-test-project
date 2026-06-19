f91dd827745d8a5d6e71f2342330a1a737f73d36
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-06-19 18:52:40 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,16 +1,1 @@
-if (ArrayCount(aCities) > 0) {
 
-    bSqlStr.AppendStr("and c.data.value(");
-
-    bSqlStr.AppendStr("'(*/custom_elems/custom_elem[name=''sity'']/value)[1]'");
-
-    bSqlStr.AppendStr(",'varchar(max)')");
-
-    bSqlStr.AppendStr(" in ('");
-
-    bSqlStr.AppendStr(ArrayMerge(aCities, "This", "', '"));
-
-    bSqlStr.AppendStr("')");
-
-    bSqlStr.AppendStr("\r\n");
-}
