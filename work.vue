349a4ba37e2c3c018e2aef56cced391d16d96cd4
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-06-24 13:33:05 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,10 @@
+| Поле                           | Успешный                      | С ошибкой          | 
+| ------------------------------ | ----------------------------- | ------------------ | 
+| `start_requests_question.type` | `"hidden"`                    | `"paragraph"`      | 
+| `edit_mail_question.type`      | `"paragraph"`                 | `"hidden"`         | 
+| `collaborator_mail.type`       | `"text"`                      | `"hidden"`         | 
+| `__submit_type__.value`        | `"start_requests_question_4"` | `"start_requests"` | 
+
+
+
 
