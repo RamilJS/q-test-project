5155d987f774a77b073c34c1d061a808829d496f
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-07-07 15:28:37 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,3 +1,17 @@
 
 LogEvent("start_adaptation", "Сотрудник: " + collaboratorDocTE.fullname);
 LogEvent("start_adaptation", "Город из карточки = [" + sCollabCity + "]");
+
+if (sCities != '') {
+    aCities = sCities.split(',');
+}
+
+LogEvent("start_adaptation", "Параметр cities = " + sCities);
+
+for (city in aCities) {
+    LogEvent("start_adaptation", "Элемент массива = [" + city + "]");
+}
+
+for (sCity in aCities) {
+    sCity = Trim(sCity);
+}
