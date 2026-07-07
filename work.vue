6885d54736ff5a090e99907280808f3af4c55880
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-07-07 18:01:07 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,12 +1,5 @@
+for (i = 0; i < ArrayCount(aCities); i++) {
+    aCities[i] = Trim(aCities[i]);
+}
 
-sCollabCity = collaboratorDocTE.custom_elems.ObtainChildByKey('sity').value;
-
-findCity = ArrayOptFind(aCities, "This == sCollabCity");
-
-LogEvent(
-    "start_adaptation",
-    "Проверка города [" + sCollabCity + "] -> " +
-    (findCity == undefined ? "НЕ НАЙДЕН" : "НАЙДЕН [" + findCity + "]")
-);
-
-if (ArrayOptFind(aCities, "This == sCollabCity") == undefined && ArrayCount(aCities) > 0 ) {
+                
