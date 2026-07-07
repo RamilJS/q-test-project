a6580e8594a34ef4ca9f53d113897a01a615e5ba
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-07-07 17:24:35 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,20 @@
 
+sCities = String(Trim(Param.cities));
+aCities = [];
+
+if (sCities != '') {
+    aCities = sCities.split(',');
+}
+
+LogEvent("start_adaptation", "Параметр cities = " + sCities);
+
+for (i = 0; i < ArrayCount(aCities); i++) {
+    LogEvent(
+        "start_adaptation",
+        i + " = [" + aCities[i] + "]"
+    );
+}
+
+for (sCity in aCities) {
+    sCity = Trim(sCity);
+}
