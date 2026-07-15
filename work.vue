e07965e1d13498f95b2535f180648ed439f51f6f
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-07-15 17:02:10 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,12 +1,12 @@
 Было (для events):
-javascriptSql_String.AppendStr("LEFT JOIN [competences]\r\n");
+Sql_String.AppendStr("LEFT JOIN [competences]\r\n");
 Sql_String.AppendStr("  ON [competences].[id] = [event].[data].value('(event/custom_elems/custom_elem[name=''f_competention'']/value)[1]', 'bigint')\r\n");
 Стало:
-javascriptSql_String.AppendStr("LEFT JOIN [competences]\r\n");
+Sql_String.AppendStr("LEFT JOIN [competences]\r\n");
 Sql_String.AppendStr("  ON [competences].[id] = TRY_CONVERT(bigint, [event].[data].value('(event/custom_elems/custom_elem[name=''f_competention'']/value)[1]', 'varchar(max)'))\r\n");
 Было (для course):
-javascriptSql_String.AppendStr("LEFT JOIN [competences]\r\n");
+Sql_String.AppendStr("LEFT JOIN [competences]\r\n");
 Sql_String.AppendStr("  ON [competences].[id] = [course].[data].value('(course/custom_elems/custom_elem[name=''f_competention'']/value)[1]', 'bigint')\r\n");
 Стало:
-javascriptSql_String.AppendStr("LEFT JOIN [competences]\r\n");
+Sql_String.AppendStr("LEFT JOIN [competences]\r\n");
 Sql_String.AppendStr("  ON [competences].[id] = TRY_CONVERT(bigint, [course].[data].value('(course/custom_elems/custom_elem[name=''f_competention'']/value)[1]', 'varchar(max)'))\r\n");
