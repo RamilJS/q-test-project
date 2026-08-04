84fc521ec7d5bef8a6ed6c3778a61d77f394bc69
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-08-04 22:11:52 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,9 +1,1 @@
-bIsSamePerson = OptInt(objDocSec.custom_elems.ObtainChildByKey("f_manager_id").value, 0)
-    == OptInt(objDocSec.custom_elems.ObtainChildByKey("f_mir_code_manager_id").value, 0)
-    && OptInt(objDocSec.custom_elems.ObtainChildByKey("f_manager_id").value, 0) != 0;
 
-sAcceptActionId = bIsSamePerson ? "02_03" : "01_02";
-sDeclineActionId = bIsSamePerson ? "02_06" : "01_05";
-
-sAcceptBody = "Согласовать заявку\n\nНе удаляйте и не изменяйте информацию ниже\n----service_data----\n" + objDocSecID + "|" + sAcceptActionId + "|" + objDocID + "\n----service_data----";
-sDeclineBody = "Отклонить заявку\n\nНе удаляйте и не изменяйте информацию ниже\n----service_data----\n" + objDocSecID + "|" + sDeclineActionId + "|" + objDocID + "\n----service_data----";
