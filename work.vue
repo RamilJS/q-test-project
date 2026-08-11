2aad55f02aeea1b2c8129d6bb6a39ef3cc08208e
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-08-11 15:27:10 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,1 +1,14 @@
 
+else {
+    aResponsibleMailArray.push({
+        responsible_person_id: Int(oRequestDoc.TopElem.custom_elems.ObtainChildByKey("f_main_collaborator").value),
+        type: 'not_manager',
+        request_id: Int(oRequestDoc.DocID),
+        person_id: Int(oRequestDoc.TopElem.person_id),
+        person_fullname: String(oRequestDoc.TopElem.person_id.ForeignElem.fullname),
+        person_position: String(oRequestDoc.TopElem.person_id.ForeignElem.position_name),
+        workflow_state_name: String(oRequestDoc.TopElem.workflow_state_name),
+        object_id: Int(oRequestDoc.TopElem.object_id),
+        object_name: String(oRequestDoc.TopElem.object_id.ForeignElem.name)
+    });
+}
