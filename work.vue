a5f3744b1db142e913a4752bf7943c5e1892bdb4
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-08-19 17:33:43 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,13 +1,36 @@
-else {
-    aResponsibleMailArray.push({
-        responsible_person_id: Int(oRequestDoc.TopElem.custom_elems.ObtainChildByKey("f_main_collaborator").value),
-        type: 'not_manager',
-        request_id: Int(oRequestDoc.DocID),
-        person_id: Int(oRequestDoc.TopElem.person_id),
-        person_fullname: String(oRequestDoc.TopElem.person_id.ForeignElem.fullname),
-        person_position: String(oRequestDoc.TopElem.person_id.ForeignElem.position_name),
-        workflow_state_name: String(oRequestDoc.TopElem.workflow_state_name),
-        object_id: Int(oRequestDoc.TopElem.object_id),
-        object_name: String(oRequestDoc.TopElem.object_id.ForeignElem.name)
-    });
+function IsEventWithoutDates(requestDoc)
+{
+    result = false;
+    try {
+        EnableLog("ramil-dates-debug", true);
+
+        oEducationMethodTE = tools.open_doc(requestDoc.TopElem.object_id).TopElem;
+        alert("oEducationMethodTE.id = " + oEducationMethodTE.id);
+        alert("oEducationMethodTE.name = " + oEducationMethodTE.name);
+
+        valueDates = oEducationMethodTE.custom_elems.ObtainChildByKey("f_vtbl_dates").value;
+        alert("valueDates = " + valueDates);
+
+        aDates = ParseJson(valueDates != "" ? valueDates : "[]");
+        alert("aDates count = " + ArrayCount(aDates));
+
+        firstDate = ArrayOptFirstElem(aDates);
+        alert("firstDate = " + tools.object_to_text(firstDate, "json"));
+
+        if (firstDate != undefined) {
+            startDate = Date(firstDate.start_date);
+            alert("startDate = " + startDate);
+            alert("Day = " + Day(startDate) + ", Month = " + Month(startDate));
+
+            if (Day(startDate) == 1 && Month(startDate) == 1)
+                result = true;
+        }
+
+        alert("IsEventWithoutDates result = " + result);
+    }
+    catch(_e) {
+        alert("IsEventWithoutDates ERROR: " + _e);
+        result = false;
+    }
+    return result;
 }
