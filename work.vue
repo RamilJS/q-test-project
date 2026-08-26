d407b87227e0ee221c65995a93c4b96fba8ca5fa
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-08-26 17:34:58 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,9 +1,9 @@
 select count(*) as still_null
 from positions p
-where p.is_active = 1
+where isnull(p.is_position_finished, 0) = 0
   and p.position_common_id is null;
 
----
-
-  select count(*) as total_common_positions
-from position_commons;
+select column_name
+from information_schema.columns
+where table_name = 'positions'
+order by ordinal_position;
