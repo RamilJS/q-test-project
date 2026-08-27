96db88d4c31b17029e95fad285e85706d78766b8
Ramil Veliev <124208968+RamilJS@users.noreply.github.com>
2026-08-27 18:02:29 +0400

    Update work.vue

--- work.vue
+++ work.vue
@@ -1,9 +1,8 @@
 select count(*) as total,
        min(modification_date) as oldest,
        max(modification_date) as newest
-from cc_mir_codes;
+from cc_mir_code;
 
-
-select table_name
-from information_schema.tables
-where table_name like '%mir_code%';
+select id, name, translate, owner_id, modification_date
+from cc_mir_code
+order by modification_date desc;
